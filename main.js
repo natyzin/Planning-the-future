ebd7fb6de2f12b9d7c241390138115108428741c
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-04-13 22:48:49 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const buttons = document.querySelectorAll(".button");
+const tabsContent = document.querySelectorAll(".tab-content");
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function () {
-        for (let j = 0; j < buttons.length; j++) {
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].onclick = function(){
+        for(let j = 0; j < buttons.length; j++){
             buttons[j].classList.remove("active");
+            tabsContent[j].classList.remove("active");
         }
         buttons[i].classList.add("active");
-    };
+        tabsContent[i].classList.add("active");
+    }
 }
